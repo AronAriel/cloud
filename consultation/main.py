f1e6056ae4abbfa8efda9215916c6a25dee54b74
Aron Shapialevich <aron.shapialevich@student.ehu.lt>
2026-04-28 18:42:48 +0300

    feat: create send/receive

--- consultation/main.py
+++ consultation/main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
 from db import SessionLocal
 from models import Consultation
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SEND_CONNECTION_STRING = os.getenv("SEND_CONNECTION_STRING")
+QUEUE_NAME = os.getenv("QUEUE_NAME")
+
 
 app = FastAPI()
+class ConsultationCreate(BaseModel):
+    pet_name: str
+    date: str
+    vet_id: int
+
+
+def send_message(message: str):
+    with ServiceBusClient.from_connection_string(SEND_CONNECTION_STRING) as client:
+        sender = client.get_queue_sender(queue_name=QUEUE_NAME)
+        with sender:
+            sender.send_messages(ServiceBusMessage(message))
+
+
+
 
 @app.get("/consultations")
 def get_consultations():
@@ -18,3 +42,20 @@ def get_consultations():
         }
         for c in consultations
     ]
+
+@app.post("/consultations")
+def create_consultation(data: ConsultationCreate):
+    db = SessionLocal()
+
+    new_item = Consultation(
+        pet_name=data.pet_name,
+        date=data.date,
+        vet_id=data.vet_id
+    )
+
+    db.add(new_item)
+    db.commit()
+
+    send_message(f"New consultation created: {new_item.id}")
+
+    return {"message": "created"}
--- feedbackvet/main.py
+++ feedbackvet/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 from db import SessionLocal
 from models import Feedback
+from azure.servicebus import ServiceBusClient
+import threading
+import time
+
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+QUEUE_NAME = os.getenv("QUEUE_NAME")
+RECEIVE_CONNECTION_STRING = os.getenv("RECEIVE_CONNECTION_STRING")
 
 app = FastAPI()
 
@@ -18,3 +30,27 @@ def get_feedbacks():
         }
         for f in feedbacks
     ]
+
+def receive_messages():
+    while True:
+        print("Listening...")
+
+        with ServiceBusClient.from_connection_string(RECEIVE_CONNECTION_STRING) as client:
+            receiver = client.get_queue_receiver(
+                queue_name=QUEUE_NAME,
+                max_wait_time=5
+            )
+
+            with receiver:
+                messages = receiver.receive_messages(max_message_count=10)
+
+                for msg in messages:
+                    print("Received:", str(msg))
+                    receiver.complete_message(msg)
+
+        time.sleep(5)
+
+@app.on_event("startup")
+def start_background():
+    thread = threading.Thread(target=receive_messages, daemon=True)
+    thread.start()
